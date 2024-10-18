6002dba00d7872bd8784901013e1e47ad6c63aaa
Abdikhafar Issack <abdikhafarissack@gmail.com>
2024-10-19 01:36:56 +0300

    toggle

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './NavBar.css'; // Import the corresponding CSS
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
