da76b832e3c21bc399b83f09ee0db0d46dd89706
Abdikhafar Issack <abdikhafarissack@gmail.com>
2024-10-19 01:17:59 +0300

    menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './NavBar.css';
+import './NavBar.css'; // Import the corresponding CSS
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,19 +11,31 @@ const NavBar = () => {
 
   return (
     <nav className="navbar">
-      <div className="hamburger" onClick={toggleMenu}>
-        ☰
+      <div className="menu-icon" onClick={toggleMenu}>
+        <i className={isOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
       </div>
-      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
-        <li><Link to="/" className="active">Home</Link></li>
-        <li><Link to="/admins">Admins</Link></li>
-        <li><Link to="/patients">Patients</Link></li>
-        <li><Link to="/doctors">Doctors</Link></li>
-        <li><Link to="/appointments">Appointments</Link></li>
-        <li><Link to="/medical-records">Medical Records</Link></li>
+      <ul className={isOpen ? 'nav-links open' : 'nav-links'}>
+        <li>
+          <Link to="/" className="active">Home</Link>
+        </li>
+        <li>
+          <Link to="/admins">Admins</Link>
+        </li>
+        <li>
+          <Link to="/patients">Patients</Link>
+        </li>
+        <li>
+          <Link to="/doctors">Doctors</Link>
+        </li>
+        <li>
+          <Link to="/appointments">Appointments</Link>
+        </li>
+        <li>
+          <Link to="/medical-records">Medical Records</Link>
+        </li>
       </ul>
     </nav>
   );
-};
+}
 
 export default NavBar;
