82cbfdfdc5f128fbe69acf3de5b5fc8c2bba93f4
Abdikhafar Issack <abdikhafarissack@gmail.com>
2024-10-19 01:11:59 +0300

    hamburger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './NavBar.css'; // Import the corresponding CSS
+import './NavBar.css';
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-      <ul>
-        <li>
-          <Link to="/" className="active">Home</Link>
-        </li>
-        <li>
-          <Link to="/admins">Admins</Link>
-        </li>
-        <li>
-          <Link to="/patients">Patients</Link>
-        </li>
-        <li>
-          <Link to="/doctors">Doctors</Link>
-        </li>
-        <li>
-          <Link to="/appointments">Appointments</Link>
-        </li>
-        <li>
-          <Link to="/medical-records">Medical Records</Link>
-        </li>
+      <div className="hamburger" onClick={toggleMenu}>
+        ☰
+      </div>
+      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <li><Link to="/" className="active">Home</Link></li>
+        <li><Link to="/admins">Admins</Link></li>
+        <li><Link to="/patients">Patients</Link></li>
+        <li><Link to="/doctors">Doctors</Link></li>
+        <li><Link to="/appointments">Appointments</Link></li>
+        <li><Link to="/medical-records">Medical Records</Link></li>
       </ul>
     </nav>
   );
-}
+};
 
 export default NavBar;
